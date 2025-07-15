1a4e52f031e6f53b1810a8f5fc26ed0321149563
DJopek <david.jopek@protonmail.com>
2025-07-15 15:06:24 +0200

    different approach to initial conditions; poincare section functions (Poincare section plot at the end; Poincare section plot after every iteration)

--- double_pendulum.py
+++ double_pendulum.py
@@ -25,32 +25,19 @@ theta_2_dot_0 = []
 
 def initial_conditions(E, number_of_initial_conditions):
 
-    for i in range(0,number_of_initial_conditions):
-        theta_1_0.append(np.random.uniform(0))
-        theta_2_0.append(np.random.uniform(-pi, pi))
-        A = 0.5*l_1**2*(m_1+m_2)
-        # condition for real discriminant
-        theta_2_dot_0_candidate = np.random.uniform(-1,1)
-        B = m_2*l_1*l_2*theta_2_dot_0_candidate*np.cos(theta_1_0[i]-theta_2_0[i])
-        C = 0.5*m_2*l_2**2*theta_2_dot_0_candidate**2-l_1*g*np.cos(theta_1_0[i])*(m_1+m_2)-l_2*m_2*g*np.cos(theta_2_0[i])+(l_1+l_2)*(m_1+m_2)*g-E
-        discriminant = B**2-4*A*C
-        if discriminant >= 0:
-            theta_2_dot_0.append(theta_2_dot_0_candidate)
-        while discriminant < 0:
-            theta_2_dot_0_candidate = np.random.uniform(-1,1)
-            B = m_2*l_1*l_2*theta_2_dot_0_candidate*np.cos(theta_1_0[i]-theta_2_0[i])
-            C = 0.5*m_2*l_2**2*theta_2_dot_0_candidate**2-l_1*g*np.cos(theta_1_0[i])*(m_1+m_2)-l_2*m_2*g*np.cos(theta_2_0[i])+(l_1+l_2)*(m_1+m_2)*g-E
-            discriminant = B**2-4*A*C
-            if discriminant >= 0:
-                theta_2_dot_0.append(theta_2_dot_0_candidate)
-                break
+    for i in range(number_of_initial_conditions):
+        theta_1_0.append(0)
+        theta_2_0.append(np.random.uniform(-pi/20, pi/20))
+        theta_2_dot_0.append(0)
         bucket = []
-        bucket.append((-B+np.sqrt(B**2-4*A*C))/(2*A))
-        bucket.append((-B-np.sqrt(B**2-4*A*C))/(2*A))
+        bucket.append(np.sqrt(2*(E+l_1*g*(m_1+m_2)+l_2*m_2*g*np.cos(theta_2_0[i])-(l_1+l_2)*(m_1+m_2)*g)/(l_1**2*(m_1+m_2))))
+        bucket.append(-np.sqrt(2*(E+l_1*g*(m_1+m_2)+l_2*m_2*g*np.cos(theta_2_0[i])-(l_1+l_2)*(m_1+m_2)*g)/(l_1**2*(m_1+m_2))))
         theta_1_dot_0.append(bucket[random.randint(0,1)])
 
-Energy = [4, 4.5, 5.5, 6.5, 7.5]
+Energy = [4, 4.5, 5.6, 6.5, 7.5]
 number_of_initial_conditions = 10
+
+total_numbers_of_initial_conditions = number_of_initial_conditions*len(Energy)
 
 for i in range(len(Energy)):
     initial_conditions(Energy[i], number_of_initial_conditions)
@@ -70,13 +57,8 @@ def E(y_1, y_2, y_3, y_4):
     total_energy = 0.5*l_1**2*y_3**2*(m_1+m_2)+0.5*m_2*l_2**2*y_4**2+m_2*l_1*l_2*y_3*y_4*np.cos(y_1-y_2)-l_1*g*np.cos(y_1)*(m_1+m_2)-l_2*m_2*g*np.cos(y_2)+(l_1+l_2)*g*(m_1+m_2)
     return total_energy
 
-for i in range(len(theta_1_0)):
-    y0 = [theta_1_0[i], theta_2_0[i], theta_1_dot_0[i], theta_2_dot_0[i]]
-    system_energy = E(y0[0], y0[1], y0[2], y0[3])
-    print(system_energy)
-
 # Poincaré section
-def Poincare_section(iterations):
+def Poincare_section_iteration(iterations):
     theta_2 = []
     theta_2_dot = []
 
@@ -96,12 +78,32 @@ def Poincare_section(iterations):
 
     plt.show()
 
+def Poincare_section(total_numbers_of_initial_conditions):
+    theta_2 = []
+    theta_2_dot = []
+
+    colors = []
+
+    for i in range(total_numbers_of_initial_conditions):
+        color = ["#"+''.join([random.choice('0123456789ABCDEF') for r in range(6)])
+                    for s in range(1)]
+        for j in range(number_of_points-1):
+            if solutions[i].y[0][j] <= 0 and solutions[i].y[0][j+1] >= 0:
+                colors.append(color[0])
+                theta_2.append(solutions[i].y[1][j+1])
+                theta_2_dot.append(solutions[i].y[3][j+1])
+
+    for m in range(len(theta_2)):
+        plt.scatter((theta_2[m]+np.pi)%(2 * np.pi) - np.pi, theta_2_dot[m], c=colors[m], s=1)
+
+    plt.show()
+
 # solving the equations of motion
 solutions = []
 
 error = 0.001
 
-for i in range(number_of_initial_conditions):
+for i in range(total_numbers_of_initial_conditions):
     y0 = [theta_1_0[i], theta_2_0[i], theta_1_dot_0[i], theta_2_dot_0[i]]
     system_energy = E(y0[0], y0[1], y0[2], y0[3])
     print(y0)
@@ -114,7 +116,9 @@ for i in range(number_of_initial_conditions):
         if np.abs(solution_energy - system_energy) > error:
             sys.exit('Maximum energy drift of {} exceeded.'.format(error))
 
-    Poincare_section(i)
+    # Poincare_section(i)
+
+Poincare_section(total_numbers_of_initial_conditions)
 
 # # theta_1(t)
 # for i in range(number_of_initial_conditions):
