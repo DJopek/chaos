2353227c3212740d20860a1ff5c2e7bf9d592f6c
DJopek <david.jopek@protonmail.com>
2025-07-18 00:20:45 +0200

    theta_2_dot_0 set to 0; Energies set by np.linspace; dense_output = True insteal; of t_eval = t; some visualisations

--- double_pendulum.py
+++ double_pendulum.py
@@ -23,8 +23,21 @@ theta_2_0 = []
 theta_1_dot_0 = []
 theta_2_dot_0 = []
 
-Energy = [4, 5, 6, 7]
-number_of_initial_conditions = 3
+# Energy = np.linspace(5,5.6,8) # [0,100] 1.
+# Energy = np.linspace(5.6,6.2,8) # [0,300] 2.
+# Energy = np.linspace(6.2,6.4,8) # [0,500] 3.
+# Energy = np.linspace(6.4,6.8,8) # [0,1000] 4.
+# Energy = np.linspace(5.9,6.3,8) # [0,700] 2-3.
+
+# Energy = np.linspace(6.3,6.8,12) # [0,1200] 3-4.
+
+# Energy = np.linspace(5.4,6.8,20) # [0,2000] 0.
+# Energy = np.linspace(5.6, 6.4, 16) # [0,700] 2_3
+# Energy = np.linspace(6.2,6.8,18) # [0,1200] 3_4.
+
+Energy = [5.865]
+
+number_of_initial_conditions = 1
 
 total_numbers_of_initial_conditions = number_of_initial_conditions*len(Energy)
 
@@ -32,7 +45,7 @@ def initial_conditions(E, number_of_initial_conditions):
 
     for i in range(number_of_initial_conditions):
         theta_1_0.append(0)
-        theta_2_0.append(np.random.uniform(-pi/20, pi/20))
+        theta_2_0.append(0)
         theta_2_dot_0.append(0)
         bucket = []
         bucket.append(np.sqrt(2*(E+l_1*g*(m_1+m_2)+l_2*m_2*g*np.cos(theta_2_0[i])-(l_1+l_2)*(m_1+m_2)*g)/(l_1**2*(m_1+m_2))))
@@ -63,6 +76,9 @@ def Poincare_section(total_numbers_of_initial_conditions):
     for i in range(total_numbers_of_initial_conditions):
         color = ["#"+''.join([random.choice('0123456789ABCDEF') for r in range(6)])
                     for s in range(1)]
+
+        number_of_points = len(solutions[i].t)
+
         for j in range(number_of_points-1):
             if solutions[i].y[0][j] <= 0 and solutions[i].y[0][j+1] >= 0:
                 colors.append(color[0])
@@ -70,7 +86,7 @@ def Poincare_section(total_numbers_of_initial_conditions):
                 theta_2_dot.append(solutions[i].y[3][j+1])
 
     for m in range(len(theta_2)):
-        plt.scatter((theta_2[m]+np.pi)%(2 * np.pi) - np.pi, theta_2_dot[m], c=colors[m], s=0.1)
+        plt.scatter((theta_2[m]+np.pi)%(2 * np.pi) - np.pi, theta_2_dot[m], c=colors[m], s=0.5)
 
     plt.xlabel(r"$\theta_2\ [rad]$")
     plt.ylabel(r"$\dot{\theta_2}\ [rad \cdot s^{-1}]$")
@@ -82,15 +98,14 @@ solutions = []
 error = 0.001
 
 # defining time interval
-t_span = [0,1000]
-t = np.linspace(0, 1000, 10000)
+t_span = [0,10]
 
 for i in range(total_numbers_of_initial_conditions):
     y0 = [theta_1_0[i], theta_2_0[i], theta_1_dot_0[i], theta_2_dot_0[i]]
     system_energy = E(y0[0], y0[1], y0[2], y0[3])
     print(y0)
     print(system_energy)
-    solutions.append(solve_ivp(f, t_span, y0, t_eval=t, rtol = 1e-12, atol = 1e-14))
+    solutions.append(solve_ivp(f, t_span, y0, dense_output=True, rtol = 1e-12, atol = 1e-14))
     number_of_points = len(solutions[i].t)
     
     for j in range(number_of_points):
@@ -99,3 +114,28 @@ for i in range(total_numbers_of_initial_conditions):
             sys.exit('Maximum energy drift of {} exceeded.'.format(error))
 
 Poincare_section(total_numbers_of_initial_conditions)
+
+x_1 = []
+x_2 = []
+y_1 = []
+y_2 = []
+
+for i in range(len(solutions[0].t)):
+    x_1.append(l_1*np.sin(solutions[0].y[0][i]))
+    x_2.append(x_1[i]+l_2*np.sin(solutions[0].y[1][i]))
+    y_1.append(-l_1*np.cos(solutions[0].y[0][i]))
+    y_2.append(y_1[i]-l_2*np.cos(solutions[0].y[1][i]))
+
+plt.plot(solutions[0].y[0], solutions[0].y[1])
+plt.xlabel(r"$\theta_1\ [rad]$")
+plt.ylabel(r"$\theta_2\ [rad]$")
+plt.show()
+
+plt.plot(x_1, y_1)
+plt.show()
+plt.plot(x_2, y_2)
+plt.show()
+
+plt.plot(x_1, y_1)
+plt.plot(x_2, y_2)
+plt.show()
